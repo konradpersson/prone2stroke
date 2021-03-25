b106c1b963604404ac53f8340022c287fa3aaf7b
Irena <i.ilvesten@hotmail.com>
2021-03-25 09:34:09 +0100

    cleaning up code

--- rf_model.py
+++ rf_model.py
@@ -1,20 +1,22 @@
-import numpy as np 
-import pandas as pd
+
 import sqlite3
-import matplotlib.pyplot as pltpippip
 import warnings
 warnings.filterwarnings('ignore')
-from sklearn.preprocessing import StandardScaler,LabelEncoder
-from imblearn.over_sampling import SMOTE #needs installing of imbalanced-learn
-#import scikitplot as skplt #needs installing of scikit-plot
-import os
-from sklearn import metrics
+#importing packages
+import numpy as np 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE #needs installing of imbalanced-learn
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score, recall_score, roc_auc_score, precision_score, f1_score
 from sklearn.model_selection import train_test_split,cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+
 
 def train_model():
     con = sqlite3.connect('prone-to-stroke-dataset.db')
@@ -38,10 +40,11 @@ def train_model():
     #fixing biased data 
     oversample = SMOTE()
     X_train_resh, y_train_resh = oversample.fit_resample(X_train, y_train.ravel())
-
-    # Scale data in pipeline 
-    rf_pipeline = Pipeline(steps = [('scale',StandardScaler()),('RF',RandomForestClassifier(random_state=42))])
-    rf_cv = cross_val_score(rf_pipeline,X_train_resh,y_train_resh,cv=10,scoring='f1')
+    
+    #Random Forest Pipeline
+    RandomForestPipeline=Pipeline([('myscaler',MinMaxScaler()),
+                     ('mypca',PCA(n_components=3)),
+                     ('randomforest_classifier',RandomForestClassifier())])
 
     #Random forest Classifier 
     model = RandomForestClassifier(max_features=2,n_estimators=100,bootstrap=True)
@@ -62,15 +65,12 @@ def train_model():
     con.close()
 
 def predict(gender, age, hypertension, heart_disease,ever_married,work_type,residence_type,avg_glucose_level,bmi,smoking_status):
-      rf_pipeline.predict_proba([[gender, age, work_type, residence_type, avg_glucose_level, bmi,smoking_status]])
+      RandomForestPipeline.predict_proba([[gender, age, work_type, residence_type, avg_glucose_level, bmi,smoking_status]])
       
-"""       Test = pd.DataFrame([[gender, age, hypertension, heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,bmi,smoking_status]])
-      exp = explainer.explain_instance(
-      data_row=Test.iloc[0], 
-      predict_fn=rf_pipeline.predict_proba
-      )
-
-      exp.show_in_notebook(show_table=True) # what to do here???? exp Class? to be continued... """
-      
+          #probability = probability_of_stroke[0, 1]
+          
+          #prediction = True if probability > 0.5 else False
+          
+          #return { "willGetStroke": prediction, "probability": probability }
       
       
