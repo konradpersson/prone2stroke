d1e6c27a03ea80c43e94e3709c678725772dd05d
Dino Ferizovic <dinoferizovic@gmail.com>
2021-03-29 16:31:46 +0200

    Adding css to input fields in StrokeInput.vue

--- frontend/src/views/StrokeInput.vue
+++ frontend/src/views/StrokeInput.vue
@@ -1,15 +1,17 @@
 <template>
   <main>
-      <h2>Patient Values</h2>
-      <form>
-        <label for="cars">Which car do you drive?</label>
-        <select id="cars" name="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="fiat">Fiat</option>
-          <option value="audi">Audi</option>
-        </select>
+    <h2>Patient Values</h2>
+      <h3>Testing probability prediction</h3>
+      <form @submit.prevent="predict">
+      <label for="gender">Gender:</label><input v-model="gender" type="text" placeholder="gender"/>
+      <input v-model="age" type="text" placeholder="age"/>
+      <input v-model="work_type" type="text" placeholder="work-type"/>
+      <input v-model="residence_type" type="text" placeholder="residence-type"/>
+      <input v-model="avg_glucose_level" type="text" placeholder="avg-glucose-level"/>
+      <input v-model="bmi" type="text" placeholder="income"/>
+      <input v-model="smoking_status" type="text" placeholder="smoking-status"/>
       </form>
+      <button>Submit</button>
 
       <h2>Prediction</h2>
       <p>45% Chance to get a stroke</p>
@@ -22,6 +24,40 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form{
+  margin: 20px;
+  display: inline-block;
+  width: 100%;
+  box-shadow: 20px;
+}
+
+input{
+  width: 50%;
+  height: 50px;
+  margin: auto;
+  border-bottom: 2px solid rgb(199, 199, 199);
+  border-right: 2px solid rgb(199, 199, 199);
+  border-left: none;
+  border-top: none;
+  background-color: white;
+  margin: 5px;
+  float: left;
+  font-size: 16px;
+  padding-left: 20px;
+  outline: none;
+}
+
+button{
+  background-color: black;
+  width: 10%;
+  font-size: 20px;
+  color: white;
+  margin: 20px 5px 5px 5px;
+  padding: 20px;
+  border: none;
+  cursor: pointer;
+  outline: none;
+}
 
 </style>
